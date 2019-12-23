dfd0df90a5072555c72d6e980262a2fd03cdab8a
David <41311088+biagiola@users.noreply.github.com>
2019-12-23 11:01:30 -0300

    routes

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-const members = require('./data/members');
-
 
 const app = express();
 
@@ -10,23 +8,8 @@ const PORT =  process.env.PORT || 5000;
 // Set static folder - index
 app.use(express.static(path.join(__dirname, 'public' )));
 
-app.get('/about', (req, res) => {
-    //res.send('<h1>Welcome to Node Express project</h1>') // send plain text
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
 
-// Gets all members
-app.get('/api/members', (req, res) => res.json(members));
-
-// Get Single Member
-app.get('/api/members/:id', (req, res) => {
-    const found = members.some( member => member.id === parseInt(req.params.id) )
-
-    if (found) {
-        res.send(members.filter(member => member.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({ msg: `Err to get the member. No member with ${req.params.id}` });
-    }
-});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
