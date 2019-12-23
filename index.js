e03c8df6c0489a430a70118c418683bd54de9506
David <41311088+biagiola@users.noreply.github.com>
2019-12-23 10:22:20 -0300

    members array added

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
+const members = require('./data/members');
+
 const app = express();
 
+const PORT =  process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+// Set static folder - index
+app.use(express.static(path.join(__dirname, 'public' )));
+
+app.get('/about', (req, res) => {
     //res.send('<h1>Welcome to Node Express project</h1>') // send plain text
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
-const PORT =  process.env.PORT || 5000;
+// Gets all members
+app.get('/api/members', (req, res) => res.json(members));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
