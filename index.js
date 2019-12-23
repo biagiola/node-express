1b2a5af259db453f2d4a61d3fc66843e7523ab23
David <41311088+biagiola@users.noreply.github.com>
2019-12-23 10:45:53 -0300

    Validation members

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ app.get('/api/members', (req, res) => res.json(members));
 
 // Get Single Member
 app.get('/api/members/:id', (req, res) => {
-    res.send(members.filter(member => member.id === parseInt(req.params.id)));
+    const found = members.some( member => member.id === parseInt(req.params.id) )
+
+    if (found) {
+        res.send(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({ msg: `Err to get the member. No member with ${req.params.id}` });
+    }
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
